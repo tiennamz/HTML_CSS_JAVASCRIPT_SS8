9d27ff04ee32d4ba417a3069156ac6b7b777167f
tiennamz <ghastnam001@gmail.com>
2026-02-25 09:23:19 +0700

    update

--- BTTH/btvn.js
+++ BTTH/btvn.js
@@ -12,7 +12,7 @@ const squad = [
 
 const displayList = (listArray) =>{
     listArray.forEach((el,index) => {
-        console.log(`${el[0]} (${el[2]}): ${el[2]} bàn thắng`);
+        console.log(`${el[0]} (${el[2]}): ${el[1]} bàn thắng`);
         
     })
 }
@@ -32,11 +32,51 @@ const searchPlayer= (listArray)=>{
         console.log("Không tìm thấy cầu thủ");
         
     }else{
-        console.log(result);
+        console.log(`${result[0]} (${result[2]}): ${result[1]} bàn thắng`);
         
     }    
 }
 
+// Chức năng 3: Lọc cầu thủ theo vị trí (Sử dụng filter)
+// - Nhập vị trí cần lọc (Ví dụ: FW, MF, DF, GK).
+// - Dùng hàm filter để tạo danh sách con.
+// - In danh sách kết quả ra màn hình.
+const filterPosition =(array)=>{
+    let position=prompt("Nhập vị trí cần lọc (Ví dụ: FW, MF, DF, GK).");
+    let result= array.filter((el)=>{
+        return el[2]===position
+    })
+    console.log(result);
+    
+}
+
+// Chức năng 4: Thống kê tổng bàn thắng (Sử dụng reduce)
+// - Dùng hàm reduce tính tổng số bàn thắng của cả đội.
+// - In kết quả: "Tổng số bàn thắng hiện tại là: ..."
+const totalGoal=(array)=>{
+    let total = array.reduce((acc,cur)=>{
+        return acc+=cur[1];
+    },0)
+    console.log(`Tổng số bàn thắng hiện tại là: ${total}`);
+    
+}
+
+// Chức năng 5: Kiểm tra hiệu suất (Sử dụng some và every)
+// - Kiểm tra có cầu thủ nào chưa ghi bàn (0 bàn) không? (Dùng some) -> In ra “Có cầu thủ chưa ghi bàn”.
+// - Kiểm tra có phải tất cả cầu thủ đều đã ghi bàn (> 0) không? (Dùng every) -> In ra " tất cả cầu thủ đều đã ghi bàn" .
+const efficiency=(array)=>{
+    if(array.some((el)=>{
+        return el[1]===0
+    })){
+        console.log("Có cầu thủ chưa ghi bàn");
+        
+    }else if(array.every((el)=>{
+        return el[1]!==0        
+    })){
+        console.log( " tất cả cầu thủ đều đã ghi bàn");
+        
+    }
+}
 
 // Menu
 let choice;
@@ -58,16 +98,16 @@ do{
             searchPlayer(squad)
             break;
         case 3:
-            
+            filterPosition(squad)
             break;
         case 4:
-            
+            totalGoal(squad)
             break;
         case 5:
-            
+            efficiency(squad)
             break;
         case 0:
-            
+            alert("=> Hẹn gặp lại!")
             break;
     
         default:
